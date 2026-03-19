a32c9ac31e641930690112f18e410b68071e2d4b
ArthurFuller <arthurlgfuller@gmail.com>
2026-03-19 17:58:39 -0300

    Fazendo a verificação de colisão utilizando um Sensor e Linecast
    Ver a função "OnFloor" na linha 55 do script "PlayerController".

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float vel;
     [SerializeField] float jumpForce;
+    [SerializeField] Transform floorSensor;
+
+    private bool onFloor;
 
     Rigidbody2D rb;
     SpriteRenderer sr;
@@ -20,6 +23,7 @@ public class PlayerController : MonoBehaviour
         Move();
         Jump();
         Orientation();
+        OnFloor();
     }
 
      public void Move()
@@ -30,7 +34,7 @@ public class PlayerController : MonoBehaviour
 
     public void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && onFloor)
         {
             rb.linearVelocity = new Vector2(0, jumpForce);
         }
@@ -46,5 +50,13 @@ public class PlayerController : MonoBehaviour
         {
             sr.flipX = true;
         }
+    }
+
+    public void OnFloor()
+    {
+        //Utilizando Linecast e LayerMask para verificar a colisao do Sensor com o Piso
+        onFloor = Physics2D.Linecast(transform.position, floorSensor.position, 1 << LayerMask.NameToLayer("Floor"));
+
+        print(onFloor);
     }
 }
